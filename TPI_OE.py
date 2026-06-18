695f33f1094d7dec3a8467d185b8a98f21a9bdda
NicolasRueda1 <nicolasmrueda@gmail.com>
2026-06-18 20:31:35 -0300

    Actualización
    Creación de funciones validar legajo, consultar saldo y Solicita Validar dias más corrección de errores.

--- TPI_OE.py
+++ TPI_OE.py
@@ -5,9 +5,9 @@ def cargar_empleados():
             next(archivo) # Hace que salte la linea del encabezado
             
             for linea in archivo:
-                legajo, nombre, saldo = linea.strip().split(",")
-                
                 try:
+                
+                    legajo, nombre, saldo = linea.strip().split(",")
                     empleado = {
                         "legajo": legajo,
                         "nombre": nombre,
@@ -33,21 +33,21 @@ def carga_solicitud():
     solicitudes = []
 
     try:
-        with open("solicitudes.csv", "r", encoding= "utf-8") as archivo:
+        with open("vacaciones.csv", "r", encoding= "utf-8") as archivo:
             next(archivo) # Saltea el encabezado
 
-            try:    
-                for linea in archivo:
+            for linea in archivo:
+                try:
+                
                     legajo, fecha, estado= linea.strip().split(",")
-
                     solicitud = {
                         "legajo": legajo,
                         "fecha": fecha,
                         "estado": estado
                         }
                     solicitudes.append(solicitud)
-                    
-            except ValueError as error: # Maneja un error en el csv, por si falta una columna
+                
+                except ValueError as error: # Maneja un error en el csv, por si falta una columna
                     print(f"Linea con formato incorrecto: {linea.strip()}")
                     
         return solicitudes
@@ -56,21 +56,62 @@ def carga_solicitud():
         print("El archivo no existe en la ubicación brindada.")
     
 def validar_legajo(empleados):
-    try:
-        id = input("Ingrese su legajo: ")
+        
+    while True:
 
-        if not id.isdigit():
-            raise ValueError ("El legajo debe ser numérico.")
-    
-    except ValueError as error:
-        print(error)
-        return False
+        try:
+            id = input("Ingrese su legajo: ")
 
+            if not id.isdigit():
+                raise ValueError ("El legajo debe ser numérico.")
+        
+        except ValueError as error:
+            print(error)
+            continue # Vuelve al inicio del programa
+
+        for empleado in empleados:
+            if empleado["legajo"] == id :
+                return id
+        
+        print ("Legajo no encontrado, intente nuevamente.")
+
+def consultar_saldo(legajo, empleados):
     for empleado in empleados:
-        if empleado["legajo"] == id :
-            return True
+        if empleado["legajo"] == legajo:
+            if empleado["saldo"] > 0:
+                print(f"Saldo disponible, cantidad de dias: {empleado['saldo']}")
+                return True
+            else:
+                return False
+            
+def SolicitarValidar_Dias(legajo, empleados):
     
-    return False
+    saldo = 0
+    
+    for empleado in empleados:
+        
+        if empleado["legajo"] == legajo:
+            saldo = empleado["saldo"]
+
+    while True:
+
+        dias=input("¿Cuantos dias de vacaciones querés solicitar?\n ")
+        
+
+        if not dias.isdigit():
+            print("Error, ingresá un numero entero válido. ")
+            continue
+
+        dias = int(dias)
+
+        if dias <= saldo:
+            print("Cantidad de dias solicitados válido.")
+            return True
+        else:
+            print("No tenés los días suficientes, intenta con menos.")
+            
+
+        
 
 
         
